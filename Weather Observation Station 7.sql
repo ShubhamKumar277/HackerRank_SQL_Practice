0a5771db24a144dea44038ab3c0ab8680f64ab55
SHUBHAM KUMAR <44528845+ShubhamKumar277@users.noreply.github.com>
2020-07-06 23:26:11 +0530

    Create Weather Observation Station 7.sql

---
+++ Weather Observation Station 7.sql
@@ -0,0 +1,5 @@
+#Link ---> https://www.hackerrank.com/challenges/weather-observation-station-7/problem
+
+select distinct CITY
+from STATION
+where CITY like '%a' or CITY like '%e' or CITY like '%i' or CITY like '%o' or CITY like '%u';

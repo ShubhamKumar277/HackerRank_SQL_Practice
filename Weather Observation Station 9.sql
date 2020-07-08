359fbf19831d45eab7a8a231833f02da01a0ec9e
SHUBHAM KUMAR <44528845+ShubhamKumar277@users.noreply.github.com>
2020-07-08 23:57:22 +0530

    Create Weather Observation Station 9.sql

---
+++ Weather Observation Station 9.sql
@@ -0,0 +1,5 @@
+# link --> https://www.hackerrank.com/challenges/weather-observation-station-9/problem
+
+select distinct CITY 
+from STATION
+where not (CITY like 'a%' or CITY like 'e%' or CITY like 'i%' or CITY like 'o%' or CITY like 'u%'); 
